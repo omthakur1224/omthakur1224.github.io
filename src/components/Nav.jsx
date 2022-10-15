373b1c42f7a05e955c19d049b89195acfb4b0fee
omthakur1224 <omthakur1224@gmail.com>
2022-10-15 07:33:26 +0530

    nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div style={{display:"flex"}}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Tech Stack</li>
+        <li>Projects</li>
+    </div>
+  )
+}
+
+export default Nav
