fed1d2880b3e8666041e111c40c2b7c7d1455dd4
omthakur1224 <omthakur1224@gmail.com>
2022-11-07 20:57:32 +0530

    navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div style={{display:"flex"}}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Tech Stack</li>
-        <li>Projects</li>
+    <div style={{display:"flex",justifyContent:"space-evenly",
+    height:"50px",
+    backgroundColor:"skyblue"
+    }}>
+      <div style={{display:"flex",justifyContent:"space-around"}}>
+        <ul>Home</ul>
+        <ul>About</ul>
+        <ul>Tech Stack</ul>
+        <ul>Projects</ul>
+      </div>
+      <div>
+        <ul>logo</ul>
+      </div>
     </div>
   )
 }
