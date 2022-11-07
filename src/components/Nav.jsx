65124e9a93cc6d7c5bc84eacdf04cfec3cbf5fa8
omthakur1224 <omthakur1224@gmail.com>
2022-11-07 21:10:52 +0530

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,16 +3,24 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <div style={{display:"flex",justifyContent:"space-evenly",
-    height:"50px",
-    backgroundColor:"skyblue"
+    <div style={{
+        display:"flex",
+        justifyContent:"space-evenly",
+        height:"50px",
+        backgroundColor:"skyblue"
     }}>
-      <div style={{display:"flex",justifyContent:"space-around"}}>
-        <Link to={'/'}> <ul> Home</ul></Link>
-        <Link to={'/about'}> <ul> About</ul></Link >
-        <Link to={'/tech-stack'}> <ul> Tech Stack</ul></Link >
-        <Link to={'/projects'}> <ul> Projects</ul></Link >
-      </div>
+
+        <div style={{display:"flex",justifyContent:"space-around"}}>
+
+          <Link to={'/'}> <ul> Home</ul></Link>
+
+          <Link to={'/about'}> <ul> About</ul></Link >
+
+          <Link to={'/tech-stack'}> <ul> Tech Stack</ul></Link >
+
+          <Link to={'/projects'}> <ul> Projects</ul></Link >
+
+        </div>
       <div>
         <ul>logo</ul>
       </div>
